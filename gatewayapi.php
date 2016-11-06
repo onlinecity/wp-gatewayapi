1a165f332b7422eae62e1e85596b2b57de169f90
Morten Skyt Eriksen <diktator@bitnissen.com>
2016-11-06 13:31:32 -0300

    version number update

--- gatewayapi.php
+++ gatewayapi.php
@@ -3,7 +3,7 @@
 Plugin Name: GatewayAPI
 Plugin URI:  https://wordpress.org/plugins/gatewayapi/
 Description: Send SMS'es through WordPress.
-Version:     1.2.0
+Version:     1.2.1
 Author:      OnlineCity ApS
 Author URI:  http://onlinecity.dk
 License:     GPLv2
