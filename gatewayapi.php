719280baae6419b32021ff9bd0de596f681f96f4
Morten Skyt Eriksen <info@bitnissen.com>
2026-02-02 14:37:57 +0100

    Refactor code for readability and improve capability upgrade logic in gateway initialization.

--- gatewayapi.php
+++ gatewayapi.php
@@ -9,75 +9,74 @@ Author URI:  http://onlinecity.dk
 License:     GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
-if (!defined('ABSPATH')) die('Cannot be accessed directly!');
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Cannot be accessed directly!' );
+}
 
 require_once( plugin_dir_path( __FILE__ ) . '/libraries/action-scheduler/action-scheduler.php' );
 
 const GATEWAYAPI_VERSION = '2.0.5';
 
-function gatewayapi__dir()
-{
-  return __DIR__;
+function gatewayapi__dir() {
+	return __DIR__;
 }
 
-function gatewayapi__url()
-{
-  static $dir;
-  if ($dir) return $dir;
+function gatewayapi__url() {
+	static $dir;
+	if ( $dir ) {
+		return $dir;
+	}
 
-  $dir = plugin_dir_url(__FILE__);
-  return $dir;
+	$dir = plugin_dir_url( __FILE__ );
+
+	return $dir;
 }
 
-add_action('init', function () {
-  $D = gatewayapi__dir();
+function gatewayapi_ensure_capabilities() {
+	if ( ! is_admin() ) {
+		return;
+	}
 
-  // Ensure gatewayapi_manage capability is added to roles with edit_posts capability
-  if (is_admin()) {
-    $current_version = get_option('gatewayapi_version');
-    if (!$current_version || version_compare($current_version, '2.0.0', '<')) {
-      $wp_roles = wp_roles();
-      foreach ($wp_roles->roles as $role_name => $role_info) {
-        if (isset($role_info['capabilities']['edit_posts']) && $role_info['capabilities']['edit_posts']) {
-          $wp_roles->add_cap($role_name, 'gatewayapi_manage');
-        }
-      }
-      update_option('gatewayapi_version', GATEWAYAPI_VERSION);
-    }
-  }
+	$current_version = get_option( 'gatewayapi_version' );
+	if ( $current_version && version_compare( $current_version, '2.0.0', '>=' ) ) {
+		return;
+	}
 
-  // public
-  require_once("$D/inc/api.php");
-  require_once("$D/inc/two-fa.php");
-  require_once("$D/inc/campaigns-scheduling.php");
-  require_once("$D/inc/woocommerce-scheduling.php");
-
-  // admin: editor required
-  if (!current_user_can('gatewayapi_manage')) return;
-
-  require_once("$D/inc/admin-ajax.php");
-  require_once("$D/inc/contacts-post-type.php");
-  require_once("$D/inc/contacts-ajax.php");
-  require_once("$D/inc/campaigns-post-type.php");
-  require_once("$D/inc/campaigns-ajax.php");
-  require_once("$D/inc/woocommerce-post-type.php");
-  require_once("$D/inc/woocommerce-ajax.php");
-  require_once("$D/inc/admin-menu.php");
-}, 9);
-
-if (defined('GATEWAYAPI_DEVSERVER')) {
-  require_once(gatewayapi__dir() . "/inc/devserver.php");
+	$wp_roles = wp_roles();
+	foreach ( $wp_roles->roles as $role_name => $role_info ) {
+		if ( isset( $role_info['capabilities']['edit_posts'] ) && $role_info['capabilities']['edit_posts'] ) {
+			$wp_roles->add_cap( $role_name, 'gatewayapi_manage' );
+		}
+	}
+	update_option( 'gatewayapi_version', GATEWAYAPI_VERSION );
 }
 
-/**
- * On activation: Add gatewayapi_manage capability to all roles with edit_posts capability.
- */
-register_activation_hook(__FILE__, function () {
-  $wp_roles = wp_roles();
-  foreach ($wp_roles->roles as $role_name => $role_info) {
-    if (isset($role_info['capabilities']['edit_posts']) && $role_info['capabilities']['edit_posts']) {
-      $wp_roles->add_cap($role_name, 'gatewayapi_manage');
-    }
-  }
-  update_option('gatewayapi_version', GATEWAYAPI_VERSION);
-});
+add_action( 'init', function () {
+	$D = gatewayapi__dir();
+
+	gatewayapi_ensure_capabilities();
+
+	// public
+	require_once( "$D/inc/api.php" );
+	require_once( "$D/inc/two-fa.php" );
+	require_once( "$D/inc/campaigns-scheduling.php" );
+	require_once( "$D/inc/woocommerce-scheduling.php" );
+
+	// admin: editor required
+	if ( ! current_user_can( 'gatewayapi_manage' ) ) {
+		return;
+	}
+
+	require_once( "$D/inc/admin-ajax.php" );
+	require_once( "$D/inc/contacts-post-type.php" );
+	require_once( "$D/inc/contacts-ajax.php" );
+	require_once( "$D/inc/campaigns-post-type.php" );
+	require_once( "$D/inc/campaigns-ajax.php" );
+	require_once( "$D/inc/woocommerce-post-type.php" );
+	require_once( "$D/inc/woocommerce-ajax.php" );
+	require_once( "$D/inc/admin-menu.php" );
+}, 9 );
+
+if ( defined( 'GATEWAYAPI_DEVSERVER' ) ) {
+	require_once( gatewayapi__dir() . "/inc/devserver.php" );
+}
